7670090c761cb1794e2ae627ca2db1a266554740
tediko <tediko123@wp.pl>
2024-03-10 16:15:19 +0100

    Added leaflet script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello');
+// Leaflet map
+import L from 'leaflet';
+
+let map = L.map('map', {
+    center: ([41.48041, -71.31091])
+}).setView([41.48041, -71.31091], 19);
+
+let locationIcon  = L.icon({
+    iconUrl: './assets/shared/icon-location.svg',
+    iconSize: [66,88],
+    iconAnchor: [33,88]
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([41.48041, -71.31091], {icon: locationIcon}).addTo(map);
