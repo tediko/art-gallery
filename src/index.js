e166cd4f4638720c377dfeda40d67a05f6b5dbd8
tediko <tediko123@wp.pl>
2024-03-11 15:15:19 +0100

    Added helper classes for leaflet elements

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ document.querySelector('#map').dataset.state = 'ready';
 // latitude and longitude of art gallery location
 const locationLatLng = [41.48041, -71.31091];
 
+// Helper classes to style leaflet elements
+const iconClassName = 'location__map-icon';
+const tileLayerClassName = 'location__map-layer';
+
 // Instantiates a map object with options
 let map = L.map('map', {
     center: (locationLatLng),
@@ -19,12 +23,14 @@ let map = L.map('map', {
 let locationIcon  = L.icon({
     iconUrl: './assets/shared/icon-location.svg',
     iconSize: [66,88],
-    iconAnchor: [33,88]
+    iconAnchor: [33,88],
+    className: iconClassName
 });
 
 // Instantiates a tile layer object
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    className: tileLayerClassName
 }).addTo(map);
 
 
