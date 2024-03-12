985713a9288e02ab0facd90c241c9f08acf3247e
tediko <tediko123@wp.pl>
2024-03-12 16:41:39 +0100

    Changed name for first entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: {'home': './src/index.js', 'location': './src/location.js'},
+    entry: {'index': './src/index.js', 'location': './src/location.js'},
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
